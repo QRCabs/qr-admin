a2df72d92d466f382fc2bb5383cee9000191cd48
AIyazMd <aiyaz.mohammed12@gmail.com>
2024-11-21 20:12:15 +0530

    vehicle_number filter

--- src/components/drivers/DriversDataTable.jsx
+++ src/components/drivers/DriversDataTable.jsx
@@ -212,7 +212,7 @@ function DriversDataTable({ data, page, onPageChange, pageSize, totalRecords, in
                               type="text"
                               name="vehicle_number"
                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
-                              value={values?.mobile}
+                              value={values?.vehicle_number}
                               onChange={handleChange}
                               onBlur={handleBlur}
                               onKeyDown={(e) => {
